132fb8646e8ef90f4009615ebf645dba67c15f77
Jose Pablo Nordenflycht <diabhlo@gmail.com>
2020-08-19 22:38:50 -0400

    listo

--- Ve.rb
+++ Ve.rb
@@ -1,5 +1,5 @@
 gravedad = ARGV[0].to_f
 radio  = ARGV[1].to_f
-vel_escape = Math.sqrt(2 * gravedad * radio)
+vel_escape = Math.sqrt(2 * gravedad * radio).round(3)
 
-puts " La velocidad de escape es #{vel_escape}"
+puts " La velocidad de escape es de #{vel_escape} Mts/segundo"
